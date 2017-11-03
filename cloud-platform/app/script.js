677bbaf4e9b223a65458ebbbf81bf8722c974e64
MengXiangge <33297435+MengXiangge@users.noreply.github.com>
2017-11-03 15:42:43 -0400

    Add files via upload

---
+++ cloud-platform/app/script.js
@@ -0,0 +1,10 @@
+function Ctrl($scope) {
+    $scope.services = [
+        {ServiceID: 1, ServiceName: 'Service1'},
+        {ServiceID: 2, ServiceName: 'Service2'},
+        {ServiceID: 3, ServiceName: 'Service3'}
+    ];
+}
+/**
+ * Created by th on 2017/7/5.
+ */
